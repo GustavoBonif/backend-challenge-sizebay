debaa92125e73f2b67e3be549286340eab9d8cb2
GustavoBonif <gubonic@gmail.com>
2023-05-14 20:05:19 -0300

    feat: create task not found message

--- source/backend/challenge/modules/task/infra/http/controllers/TaskController.java
+++ source/backend/challenge/modules/task/infra/http/controllers/TaskController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Path("tasks")
 public class TaskController {
 
+	private final String task_not_found_alert = "Task not found!";
 	private final ICreateTaskService createTaskService;
 	private final IDeleteTaskService deleteTaskService;
 	private final IUpdateTaskService updateTaskService;
@@ -50,6 +51,11 @@ public class TaskController {
 	public Response index(@PathParam("taskId") Long taskId) {
 		try {
 			Task requestedTask = retrieveTaskByIdService.execute(taskId);
+
+			if (requestedTask == null) {
+				return DefaultResponse.ok().entity(task_not_found_alert);
+			}
+
 			return DefaultResponse.ok().entity(requestedTask);
 		}
 		catch (Exception e) {
@@ -75,6 +81,11 @@ public class TaskController {
 	public Response update(@PathParam("taskId") Long taskId, Task task) {
 		try {
 			Task taskToUpdate = retrieveTaskByIdService.execute(taskId);
+
+			if (taskToUpdate == null) {
+				return DefaultResponse.ok().entity(task_not_found_alert);
+			}
+
 			taskToUpdate.setTitle(task.getTitle());
 			taskToUpdate.setDescription(task.getDescription());
 			Task updatedTask = updateTaskService.execute(taskToUpdate);
@@ -89,6 +100,11 @@ public class TaskController {
 	public Response delete(@PathParam("taskId") Long taskId) {
 		try {
 			Task taskToBeDeleted = retrieveTaskByIdService.execute(taskId);
+
+			if (taskToBeDeleted == null) {
+				return DefaultResponse.ok().entity(task_not_found_alert);
+			}
+
 			deleteTaskService.execute(taskToBeDeleted.getId());
 			return DefaultResponse.ok().entity("Task deleted!");
 		} catch (Exception e) {
